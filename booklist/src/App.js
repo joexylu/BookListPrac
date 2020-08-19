bf3849b9fb535ba0cb5cc73df83f5bb6a7319599
joexylu <joe.luxiyang@gmail.com>
2020-08-18 19:13:55 -0700

    finish react hook beginner level

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import BookContextProvider from './contexts/BookContext';
+import BookList from './components/BookList';
+import NewBookForm from './components/NewBookForm';
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <Navbar />
+        <BookList />
+        <NewBookForm />
       </BookContextProvider>
     </div>
   );
